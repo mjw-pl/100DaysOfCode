79fcb3ce923a1d9b30be48834ef7cf82c40bcc1b
Mariusz Jakubowski <mariusz@mjw.pl>
2022-08-24 22:41:33 +0200

    Day 005 - .NET MAUI Blazor

---
+++ Day005/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Day005;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Day005/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Day005;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
